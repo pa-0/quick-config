133a1d01fccee9cd371f562fefb0b33dba6ae553
DarwinJS <DarwinJS@users.noreply.github.com>
2015-03-28 16:39:24 -0400

    Update QuickConfigPowerShell5.ps1

--- QuickConfigPowerShell5.ps1
+++ QuickConfigPowerShell5.ps1
@@ -3,10 +3,6 @@
 
 "Getting Started..." | out-default
 
-"`r`n`r`nATTENTION: Setting Machine Execution Policy to Remote-Signed`r`n`r`n" | out-default
-Try {set-executionpolicy RemoteSigned -Force -EA Silently Continue}
-Catch{}
-
 If (!(Test-Path env:ChocolateyInstall))
   {
   "Installing Chocolatey Package Manager..." | out-default
@@ -20,7 +16,8 @@ Choco Install -y PowerShell -Pre
 "Installing PowerShell DSC Resource Kit..." | out-default
 Choco Install -y DSCResourcekit
 
-"`r`n`r`nATTENTION: Your Machine Execution Policy was Set to RemoteSigned`r`n`r`n" | out-default
+"`r`n`r`nATTENTION: If this is a test or lab machine, do not forget to set your PowerShell Execution policy to RemoteSigned to allow scripts to run using `"Set-ExecutionPolicy RemoteSigned -Force`"`r`n`r`n" | out-default
 
-"Please resetart for WMF / PowerShell 5 to become active." | out-default
+
+"Please resetart for WMF / PowerShell 5 to become active. (In PowerShell: `"Restart-Computer`")" | out-default
 
