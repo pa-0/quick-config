797108bc915ccd7c07ccfc137ea58645608787ee
DarwinJS <DarwinJS@users.noreply.github.com>
2015-03-28 16:16:16 -0400

    Create PowerShell5.cmd

---
+++ PowerShell5.cmd
@@ -0,0 +1,1 @@
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/CSI-Windowscom/Bootstraps/master/PowerShell5.ps1'))"
