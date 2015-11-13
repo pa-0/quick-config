4a9e3253dd6ab01b901dfe5f781c0f98a62a1757
DarwinJS <darwinjsgitlab.services@sanoys.com>
2015-11-13 19:22:37 +0000

    d

---
+++ diagbrowser.cmd
@@ -0,0 +1,1 @@
+#powershell -NoProfile -ExecutionPolicy unrestricted -Command "[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {[bool]1};iex ((new-object net.webclient).DownloadString('https://gitlab.com/CSI-Windowscom/quick-config/raw/master/diagbrowser.ps1'))"
