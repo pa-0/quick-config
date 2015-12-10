5ec684d2075871e32f49d767a706632396604391
DarwinJS <darwinjsgitlab.services@sanoys.com>
2015-12-10 17:00:54 +0000

    first

---
+++ installchocolatey.ps1
@@ -0,0 +1,45 @@
+Function Console-Prompt {
+  Param( [String[]]$choiceList,[String]$Caption = "Please make a selection",[String]$Message = "Choices are presented below",[int]$default = 0 )
+$choicedesc = New-Object System.Collections.ObjectModel.Collection[System.Management.Automation.Host.ChoiceDescription] 
+$choiceList | foreach { 
+$comps = $_ -split '=' 
+$choicedesc.Add((New-Object "System.Management.Automation.Host.ChoiceDescription" -ArgumentList $comps[0],$comps[1]))} 
+#$choicedesc.Add((New-Object "System.Management.Automation.Host.ChoiceDescription" -ArgumentList $_))} 
+$Host.ui.PromptForChoice($caption, $message, $choicedesc, $default) 
+}
+
+Write-output "`r`n`r`nQuick Config by Darwin (CSI-Windows.com)...`r`n`r`n"
+Write-output "Gets Your Machine Ready to Author Chocolatey Packages"
+
+"Getting Started..." | out-default
+
+Set-ExecutionPolicy RemoteSigned
+
+$os = (Get-WmiObject "Win32_OperatingSystem")
+
+If (!(Test-Path env:ChocolateyInstall))
+  {
+  "Installing Chocolatey Package Manager..." | out-default
+  iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
+  $env:path = "$($env:ALLUSERSPROFILE)\chocolatey\bin;$($env:Path)"
+  }
+
+Write-Output "Installing Packages"
+
+$gitpath = 'C:\Program Files (x86)\git\cmd'
+$CurrentMachinePath = [Environment]::GetEnvironmentVariable("Path", "Machine")
+$CurrentProcessPath = [Environment]::GetEnvironmentVariable("Path", "Process")
+if (!($CurrentMachinePath -ilike "*\git\cmd*"))
+  {
+  [Environment]::SetEnvironmentVariable("Path", $CurrentMachinePath + ";$gitpath", "Machine")
+  }
+
+if (!($CurrentProcessPath -ilike "*\git\cmd*"))
+  {
+  [Environment]::SetEnvironmentVariable("Path", $CurrentProcessPath + ";$gitpath", "Process")
+  }
+git config --global credential.helper wincred
+
+choco install nuget.client
+choco install nuget.commandline
+$env:path = $env:path + ";C:\Program Files (x86)\git\cmd"
