e07598a7f0135f4057fa6ecc6947040f8f9049e5
DarwinJS <darwinjsgitlab.services@sanoys.com>
2015-12-11 14:05:37 +0000

    update

--- installchocolatey.ps1
+++ installchocolatey.ps1
@@ -1,5 +1,7 @@
-#powershell -NoProfile -ExecutionPolicy unrestricted -Command "[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {[bool]1};iex ((new-object net.webclient).DownloadString('https://gitlab.com/CSI-Windowscom/quick-config/raw/master/installchocolatey.ps1'))"
-
+#Instructions:
+#  1) Open an ELEVATED CMD.Exe Prompt
+#  2) Drop in this command: @powershell -NoProfile -ExecutionPolicy unrestricted -Command "[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {[bool]1};iex ((new-object net.webclient).DownloadString('https://gitlab.com/CSI-Windowscom/quick-config/raw/master/installchocolatey.ps1'))"
+#
 Function Console-Prompt {
   Param( [String[]]$choiceList,[String]$Caption = "Please make a selection",[String]$Message = "Choices are presented below",[int]$default = 0 )
 $choicedesc = New-Object System.Collections.ObjectModel.Collection[System.Management.Automation.Host.ChoiceDescription] 
