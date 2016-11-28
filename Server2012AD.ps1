86b6b34e2d2f5e3523f53fc55bc614c5348bd0b4
DarwinJS <darwinjsgitlab.services@sanoys.com>
2016-11-28 20:39:45 +0000

    Update Server2012AD.ps1

--- Server2012AD.ps1
+++ Server2012AD.ps1
@@ -3,7 +3,7 @@
 #powershell -NoProfile -ExecutionPolicy remotesigned -Command "[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {[bool]1};iex ((new-object net.webclient).DownloadString('https://gitlab.com/CSI-Windowscom/quick-config/raw/master/Server2012AD.ps1'))"
 
 
-Import-module servermanager]
+Import-module servermanager
 Add-WindowsFeature AD-Domain-Services
 
 Install-addsforest -domainname qqontoso.com -safemodeadministratorpassword (convertto-securestring "Admin4Domain!" -asplaintext -force) -domainmode Win2012 -domainnetbiosname contoso -forestmode Win2012
