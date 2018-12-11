5e1ba7d94fad65e84d3f80c33006d122cc0cc17f
DarwinJS <darwinjs@sanoys.com>
2018-12-11 12:22:32 +0000

    Update linuxmintconfig.sh

--- linuxmintconfig.sh
+++ linuxmintconfig.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-echo "Installing PowerShell and VS Code"
-wget https://raw.githubusercontent.com/DarwinJS/PowerShell/issue-8437-installpsh-debian-support-for-linuxmint/tools/installpsh-debian.sh
-chmod 755 installpsh-debian.sh
-sudo ./installpsh-debian.sh -includeide
-
-echo "Installing Chrome"
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+if [[ ! -z "$(sudo fuser /var/lib/dpkg/lock)" ]]; then
+  echo "Package Manager is in use, try again later, exiting..."
+fi
 
 echo "Removing Chromium Browser"
 sudo apt-get purge chromium-browser  
@@ -16,3 +11,15 @@ rm -rf ~/.config/chromium
 echo "Removing Firefox"
 sudo apt-get purge firefox
 rm -rf ~/.mozilla/firefox
+
+echo "Installing PowerShell and VS Code"
+wget https://raw.githubusercontent.com/DarwinJS/PowerShell/issue-8437-installpsh-debian-support-for-linuxmint/tools/installpsh-debian.sh
+chmod 755 installpsh-debian.sh
+sudo ./installpsh-debian.sh -includeide
+
+echo "Installing Git"
+sudo apt-get install git -y
+
+echo "Installing Chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
