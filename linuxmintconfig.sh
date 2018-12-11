3172d2a25d7b447ce1516797528c065a2e2704e0
DarwinJS <darwinjs@sanoys.com>
2018-12-11 12:07:57 +0000

    linuxmintconfig.sh

---
+++ linuxmintconfig.sh
@@ -0,0 +1,13 @@
+
+echo "Installing PowerShell and VS Code"
+wget https://github.com/DarwinJS/PowerShell/blob/issue-8437-installpsh-debian-support-for-linuxmint/tools/installpsh-debian.sh
+chmod 755 installpsh-debian.sh
+sudo ./installpsh-debian.sh -includeide
+
+echo "Installing Chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+echo "Removing Chromium Browser"
+sudo apt-get purge chromium-browser  
+rm -rf ~/.config/chromium
