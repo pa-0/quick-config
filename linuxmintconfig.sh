e40673b4cfbd717db7659c2304a7438826233506
DarwinJS <darwinjs@sanoys.com>
2018-12-12 00:42:43 +0000

    Update linuxmintconfig.sh

--- linuxmintconfig.sh
+++ linuxmintconfig.sh
@@ -22,6 +22,13 @@ if [ $(dpkg-query -W -f='${Status}' ${PKG} 2>/dev/null | grep -c "ok installed")
   sudo dpkg -i google-chrome-stable_current_amd64.deb
 fi
 
+if [[ grep -c 208.67.222.222 /etc/dhcp/dhclient.conf -eq 0 ]]; then
+  echo 'Setting up OpenDNS'
+  echo 'supersede domain-name-servers 208.67.222.222,208.67.220.220;' | \
+    sudo tee --append /etc/dhcp/dhclient.conf
+  sudo service network-manager restart
+fi
+
 if [[ "'$*'" =~ devtools ]] ; then
     echo "Installing Development Tools due to switch '-devtools'"
     PKG=powershell
