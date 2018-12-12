18cce40ae383689c454e5173acfe62d88fad60d7
DarwinJS <darwinjs@sanoys.com>
2018-12-12 02:07:52 +0000

    Update linuxmintconfig.sh

--- linuxmintconfig.sh
+++ linuxmintconfig.sh
@@ -7,13 +7,19 @@ if [[ ! -z "$(sudo fuser /var/lib/dpkg/lock)" ]]; then
   echo "Package Manager is in use, try again later, exiting..."
 fi
 
-echo "Removing Chromium Browser"
-sudo apt-get purge chromium-browser -y
-rm -rf ~/.config/chromium
+PKG=chromium-browser
+if [ $(dpkg-query -W -f='${Status}' ${PKG} 2>/dev/null | grep -c "ok installed") -gt 0 ]; then
+  echo "Removing Chromium Browser"
+  sudo apt-get purge chromium-browser -y
+  rm -rf ~/.config/chromium
+fi
 
-echo "Removing Firefox"
-sudo apt-get purge firefox -y
-rm -rf ~/.mozilla/firefox
+PKG=firefox
+if [ $(dpkg-query -W -f='${Status}' ${PKG} 2>/dev/null | grep -c "ok installed") -gt 0 ]; then
+  echo "Removing Firefox"
+  sudo apt-get purge firefox -y
+  rm -rf ~/.mozilla/firefox
+fi
 
 PKG=google-chrome-stable
 if [ $(dpkg-query -W -f='${Status}' ${PKG} 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
