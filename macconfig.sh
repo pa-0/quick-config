07ac61b4d9b4d3799efa63922abbc5c26383cac2
DarwinJS <darwinjs@sanoys.com>
2019-10-04 11:38:10 +0000

    Update macconfig.sh

--- macconfig.sh
+++ macconfig.sh
@@ -31,20 +31,6 @@ brew cask install typora copyq
 exit
 #done
 
-sudo apt-get update
-
-
-wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
-echo -e "\ndeb [arch=i386,amd64] https://deb.opera.com/opera-stable/ stable non-free" | sudo tee -a /etc/apt/sources.list
-sudo apt update
-sudo apt install -y opera-stable
-
-echo "Installing Cool Retro Term..."
-
-sudo add-apt-repository -y  ppa:vantuz/cool-retro-term
-sudo apt-get update
-sudo apt install -y cool-retro-term
-
 sudo add-apt-repository -y ppa:nathan-renniewaldock/flux
 sudo apt-get update
 sudo apt-get install -y fluxgui
@@ -73,5 +59,3 @@ echo "commands to setup a cluster (from https://itnext.io/starting-local-kuberne
 
 echo "sudo kind create cluster"
 echo "sudo kind list cluster"
-
-
