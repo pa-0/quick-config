dc1b50deed449a9f1f6b8e9065cf92ffddc0752c
DarwinJS <dsanoy@gitlab.com>
2023-12-28 18:59:55 +0000

    Updates

--- ChocolateyPackaging.ps1
+++ ChocolateyPackaging.ps1
@@ -1,3 +1,7 @@
+
+
+# Invoke-Expression -command "Invoke-WebRequest -uri 'https://gitlab.com/DarwinJS/quick-config/-/raw/master/ChocolateyPackaging.ps1?ref_type=heads' -UseBasicParsing -OutFile ./ChocolateyPackaging.ps1" ; . ./ChocolateyPackaging.ps1
+
 Function Console-Prompt {
   Param( [String[]]$choiceList,[String]$Caption = "Please make a selection",[String]$Message = "Choices are presented below",[int]$default = 0 )
 $choicedesc = New-Object System.Collections.ObjectModel.Collection[System.Management.Automation.Host.ChoiceDescription] 
@@ -13,9 +17,10 @@ Write-output "Gets Your Machine Ready to Author Chocolatey Packages"
 
 "Getting Started..." | out-default
 
-Set-ExecutionPolicy RemoteSigned
+#Set-ExecutionPolicy RemoteSigned -Force
 
 $os = (Get-WmiObject "Win32_OperatingSystem")
+
 
 If (!(Test-Path env:ChocolateyInstall))
   {
@@ -25,20 +30,35 @@ If (!(Test-Path env:ChocolateyInstall))
   }
 
 Write-Output "Installing Packages"
-choco install -y git vscode
-$gitpath = 'C:\Program Files\git\cmd'
-$CurrentMachinePath = [Environment]::GetEnvironmentVariable("Path", "Machine")
-$CurrentProcessPath = [Environment]::GetEnvironmentVariable("Path", "Process")
-if (!($CurrentMachinePath -ilike "*\git\cmd*"))
-  {
-  [Environment]::SetEnvironmentVariable("Path", $CurrentMachinePath + ";$gitpath", "Machine")
-  }
+If (!(Test-Path 'C:\Program Files\git\usr\bin\ssh-keygen.exe'))
+{
+  choco install -y git
+  $gitpath = 'C:\Program Files\git\cmd'
+  $CurrentMachinePath = [Environment]::GetEnvironmentVariable("Path", "Machine")
+  $CurrentProcessPath = [Environment]::GetEnvironmentVariable("Path", "Process")
+  if (!($CurrentMachinePath -ilike "*\git\cmd*"))
+    {
+    [Environment]::SetEnvironmentVariable("Path", $CurrentMachinePath + ";$gitpath", "Machine")
+    }
 
-if (!($CurrentProcessPath -ilike "*\git\cmd*"))
-  {
-  [Environment]::SetEnvironmentVariable("Path", $CurrentProcessPath + ";$gitpath", "Process")
+  if (!($CurrentProcessPath -ilike "*\git\cmd*"))
+    {
+    [Environment]::SetEnvironmentVariable("Path", $CurrentProcessPath + ";$gitpath", "Process")
+    }
+  git config --global credential.helper wincred
+
+  If (!(Test-Path $env:userprofile\.ssh\id_rsa.pub))
+  { 
+    Write-Host 'No default ssh key present in $env:userprofile\.ssh, generating a new one.'
+    Write-Warning 'Press enter for default file name and twice for password to set it to not have a password'
+    # & 'C:\Program Files\git\usr\bin\ssh-keygen.exe' -t rsa -f $env:userprofile/.ssh/id_rsa -q -N '""'
+    & 'C:\Program Files\git\usr\bin\ssh-keygen.exe' -t rsa -q -N '""'
   }
-git config --global credential.helper wincred
+  get-content $env:userprofile\.ssh\id_rsa.pub | clip
+  write-host "Your public ssh key is now on your clipboard, ready to be pasted into your git server at $YourGitServerhttpURL"
+}
+
+choco install -y vscode
 
 #choco install nuget.commandline
 #$env:path = $env:path + ";C:\Program Files (x86)\git\cmd"
